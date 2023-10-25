7d84fd540f46de1c170f2691400696f66bae3973
catherinealdana <catherinealdana16@gmail.com>
2023-10-25 17:59:38 -0400

    header.js_component

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Header from './components/Header';
 import Hero from './components/Hero';
 import About from './components/About';
 import Projects from './components/Projects';
-import Blog from './components/Blog';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
 
@@ -15,7 +14,6 @@ function App() {
     <Hero />
     <About />
     <Projects />
-    <Blog />
     <Contact />
     <Footer />
     </>
